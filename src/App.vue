347403d0a4153efc44f8486dbc663b8d494f2e00
Soficius <103771506+Soficius@users.noreply.github.com>
2022-07-28 22:25:04 +0200

    added call api movies

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="searchbar">
-    <input type="text" name="" id="" placeholder="cerca un film">
-    <button type="submit">cerca</button>
+    <input v-model="searchText" type="text" name="" id="" placeholder="cerca un film">
+    <button type="submit" @click.prevent="getMovies">cerca</button>
   </div>
 </template>
 
@@ -11,8 +11,31 @@ export default {
   name: 'App',
   components: {
   },
-  mounted () {
-    console.log('axios:', axios)
+  data () {
+    return {
+      movies: [],
+      searchText: '',
+      api: 'https://api.themoviedb.org/3/search/movie'
+    }
+  },
+  methods: {
+    getMovies () {
+      const config = {
+        params: {
+          api_key: '50df027645bf57ccc3ef82b89c1c311b',
+          query: this.searchText
+        }
+      }
+      this.movies = []
+      axios
+        .get(this.api, config)
+        .then((res) => {
+          this.movies.push(...res.data.results)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
   }
 }
 </script>
