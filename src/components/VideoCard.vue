f33a52c9747d39a330684addfe142be1d19e752c
Soficius <103771506+Soficius@users.noreply.github.com>
2022-08-14 19:04:26 +0200

    milestone 4 finita

--- src/components/VideoCard.vue
+++ src/components/VideoCard.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="card d-flex" :style="{backgroundImage: 'url('+video.poster+')'}" @mouseenter="hover = true" @mouseleave="hover=false">
+  <div class="card d-flex" :style="{backgroundImage: 'url('+video.poster+')'}" @mouseenter="hover = true"
+    @mouseleave="hover=false">
     <div class="text-white b-sm-gray h-100 text-center d-flex flex-column justify-content-center" v-if="hover">
       <h3>{{video.title}}</h3>
       <div>Titolo Originale: {{video.originalTitle}}</div>
-      <div>Lingua: <FlagIcon :lang="video.language"/></div>
-      <div>Voto: {{video.vote}}</div>
+      <div>Lingua:
+        <FlagIcon :lang="video.language" />
+      </div>
+      <div>Voto: <span v-for="n in 5" :key="'s'-n" :class="n <= stars ? 'e-yellow' : 'e-white'">★</span></div>
     </div>
   </div>
 </template>
@@ -19,11 +22,17 @@ export default {
   },
   components: {
     FlagIcon
+  },
+  computed: {
+    stars () {
+      return Math.ceil(this.video.vote / 2)
+    }
   }
 }
 </script>
 
 <style>
+
 .card{
   min-height: 513px;
   width: 342px;
@@ -35,5 +44,13 @@ export default {
 }
 .b-sm-gray{
   background-color: rgba(30,30,30,0.6);
+}
+.e-yellow{
+    color: transparent;
+    text-shadow: 0 0 0 yellow;
+}
+.e-white{
+    color: transparent;
+      text-shadow: 0 0 0 white;
 }
 </style>
