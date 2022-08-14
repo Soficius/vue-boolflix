8e463d3474fe898e1f2f6e3eb63739fb61f96346
Soficius <103771506+Soficius@users.noreply.github.com>
2022-08-14 16:50:10 +0200

    milestone 1-2

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,85 @@
 <template>
-  <div class="searchbar">
-    <input v-model="searchText" type="text" name="" id="" placeholder="cerca un film">
-    <button type="submit" @click.prevent="getMovies">cerca</button>
+  <div>
+    <div class="searchbar">
+      <input v-model="searchText" @keyup.enter="getVideos" type="text" name="" id="" placeholder="cerca un film o serie tv">
+      <button type="submit" @click.prevent="getVideos">cerca</button>
+    </div>
+    <main>
+      <ul v-for="video in videos" :key="video.id">
+        <li>{{video.title}}</li>
+        <li>{{video.originalTitle}}</li>
+        <li>
+          <FlagIcon :lang="video.language" />
+        </li>
+        <li>{{video.vote}}</li>
+      </ul>
+    </main>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import FlagIcon from './components/FlagIcon.vue'
 export default {
   name: 'App',
   components: {
+    FlagIcon
   },
   data () {
     return {
-      movies: [],
+      videos: [],
       searchText: '',
-      api: 'https://api.themoviedb.org/3/search/movie'
+      apiMovies: 'https://api.themoviedb.org/3/search/movie',
+      apiSeries: 'https://api.themoviedb.org/3/search/tv'
     }
   },
   methods: {
-    getMovies () {
-      const config = {
+    getVideos () {
+      this.videos = []
+      const cMovies = {
         params: {
           api_key: '50df027645bf57ccc3ef82b89c1c311b',
           query: this.searchText
         }
       }
-      this.movies = []
+      const cSeries = {
+        params: {
+          api_key: 'e99307154c6dfb0b4750f6603256716d',
+          query: this.searchText
+        }
+      }
       axios
-        .get(this.api, config)
+        .get(this.apiMovies, cMovies)
         .then((res) => {
-          this.movies.push(...res.data.results)
+          const mapped = res.data.results.map((movie) => {
+            return {
+              id: 'm-' + movie.id,
+              type: 'Movies',
+              title: movie.title,
+              originalTitle: movie.original_title,
+              language: movie.original_language,
+              vote: movie.vote_average
+            }
+          })
+          this.videos.push(...mapped)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      axios
+        .get(this.apiSeries, cSeries)
+        .then((res) => {
+          const mapped = res.data.results.map((serie) => {
+            return {
+              id: 's-' + serie.id,
+              type: 'Series',
+              title: serie.name,
+              originalTitle: serie.original_name,
+              language: serie.original_language,
+              vote: serie.vote_average
+            }
+          })
+          this.videos.push(...mapped)
         })
         .catch((err) => {
           console.log(err)
@@ -38,8 +87,7 @@ export default {
     }
   }
 }
+
 </script>
-
 <style lang="scss">
-
 </style>
---
+++ src/components/FlagIcon.vue
@@ -0,0 +1,20 @@
+<template>
+    <span :class="'fi fi-'+language"></span>
+</template>
+
+<script>
+import 'flag-icons/css/flag-icons.min.css'
+
+export default {
+  props: ['lang'],
+  computed: {
+    language () {
+      return this.lang === 'en' ? 'us' : this.lang
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
